9742ce38d588ab25e0331ad66d66d99df4decaea
anandadwii <namikaze.anandadwi@gmail.com>
2023-03-21 20:41:22 +0700

    add : cache model function and preprocessing function

---
+++ app/utils/cleaner.py
@@ -0,0 +1,65 @@
+import emoji
+import re
+from nltk.tokenize import word_tokenize
+import string
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+import streamlit as st
+from keras.models import load_model
+
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+
+def remove_emoji():
+    """menghilangkan emoji"""
+    emojis = sorted(emoji.EMOJI_DATA, key=len, reverse=True)
+    pattern = u'(' + u'|'.join(re.escape(u) for u in emojis) + u')'
+    return re.compile(pattern)
+
+
+def tokenizing_text(value):
+    result = word_tokenize(value)
+    return result
+
+
+def cleaning_text(value):
+    result = value.lower().strip()
+    # result = remove_three_same_char(result)
+    result = ' '.join(result.split())
+    result = re.sub(r'(@|https?)\S+|#[A-Za-z0-9_]+', '', result).replace("&amp;", "dan")
+    result = re.sub(r'RT[\s]+', '', result)
+    result = begone_emoji.sub(repl='', string=result)
+    result = re.sub(r'[0-9]+', '', result)
+    result = result.replace('\n', ' ')
+    result = result.translate(str.maketrans('', '', string.punctuation))
+    return result
+
+
+def filtering_stopwords(value):
+    list_stopwords = set(stopwords.words('indonesian'))
+    filtered = []
+    for text in value:
+        if text not in list_stopwords:
+            filtered.append(text)
+    return filtered
+
+
+def stemming_text(value):
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    return [stemmer.stem(word) for word in value]
+
+
+def sentence_make(tokenized):
+    return ' '.join(word for word in tokenized)
+
+
+@st.cache_resource
+def model_loader():
+    return load_model('app/resources/model/model.h5')
+
+
+begone_emoji = remove_emoji()
+
+
