0488ca3b484d13e69e097a2135d5032c9216833d
anandadwii <namikaze.anandadwi@gmail.com>
2023-03-21 14:16:36 +0700

    add : backup page

--- app/pages/main_page.py
+++ app/pages/main_page.py
@@ -1,15 +1,19 @@
 import streamlit as st
 
-st.title("Analisis Sentimen")
+st.header("Analisis Sentimen")
 st.markdown(
     """
     sentiment analysis app for e-ticket system using LSTM model
     """
 )
-st.sidebar.markdown("# Main page 🎈")
+# st.sidebar.markdown("# Main page 🎈")
 
+# with st.expander('Text Analysis'):
+#     text = st.text_input('Kalimat: ')
 
-file = st.file_uploader("upload xlsx or csv file")
+with st.expander('Batch Analysis'):
+    file = st.file_uploader("Upload CSV file with ; separated")
+
 
 
 
