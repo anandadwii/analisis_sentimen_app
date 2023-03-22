b783949c090764488e4f40d3e969b0a767ef1b1b
anandadwii <namikaze.anandadwi@gmail.com>
2023-03-22 22:21:00 +0700

    refactor : design pattern function and cleaning code

--- app/pages/main_page_alt.py
+++ app/pages/main_page_alt.py
@@ -1,21 +1,25 @@
 # allow python to jump read another python file
 import os
 import sys
-sys.path.insert(1, os.getcwd())
-
 import streamlit as st
 import pickle
 import numpy as np
 from keras.utils import pad_sequences
 import pandas as pd
 import matplotlib.pyplot as plt
+
+sys.path.insert(1, os.getcwd())
 from app.utils.cleaner import *
+from app.utils.charts import pie_chart, bar_chart
+import app.utils.page_style as ps
 seed = 10
 np.random.seed(seed)
 # start render
 st.set_page_config(page_title="Sentimen Analisis", page_icon="🤖")
 
 st.title("Analisis Sentimen")
+
+ps.remove_footer()
 st.markdown(
     """
     Aplikasi Website Mini untuk melakukan analisis sentimen dengan dataset sistem tilang elektronik di twitter.
@@ -35,23 +39,11 @@ if selectbox == "Raw Text":
     input_text = st.text_area(label='Sentimen', max_chars=100, height=100)
     trigger = st.button('Analisis!')
     if trigger:
-        raw_text = cleaning_text(input_text)
-        raw_text = tokenizing_text(raw_text)
-        # raw_text = filtering_stopwords(raw_text)
-        raw_text = stemming_text(raw_text)
-        raw_text = sentence_make(raw_text)
-        # print(raw_text)
-        sequences = tokenizer.texts_to_sequences([raw_text])
-        flat_ls = []
-        for i in sequences:
-            for j in i:
-                flat_ls.append(j)
+        clean_text = preprocessing_data(input_text)
+        sequences = tokenizer.texts_to_sequences([clean_text])
         data = pad_sequences(sequences, maxlen=53)
-
-        # print(data)
         # prediction = model.predict(np.array(data, ndmin=2))
         prediction = model.predict([data], verbose=0)
-        print(prediction)
         prediction_prob_negative = prediction[0][0]
         prediction_prob_neutral = prediction[0][1]
         prediction_prob_positive = prediction[0][2]
@@ -72,23 +64,22 @@ elif selectbox == 'csv':
     trigger = st.button('upload')
     if trigger:
         if file is not None:
-            with st.spinner('Loading'):
-                try:
+            try:
+                with st.spinner('Sedang Memproses'):
+                    # my_bar = st.progress(0.0, text='sedang membaca csv')
                     df = pd.read_csv(file, sep=';')
                     df['processed'] = df.iloc[:, 0]
-                    df['processed'] = df['processed'].apply(cleaning_text)
-                    df['processed'] = df['processed'].apply(tokenizing_text)
-                    df['processed'] = df['processed'].apply(stemming_text)
-                    X = df['processed'].apply(sentence_make)
+                    # my_bar.progress(0.25, 'sedang praproses csv')
+                    X = df['processed'].apply(preprocessing_data)
+                    # my_bar.progress(0.50, 'sedang tokenisasi')
                     X = tokenizer.texts_to_sequences(X.values)
                     X = pad_sequences(X, maxlen=53)
+                    # my_bar.progress(0.75, 'sedang melakukan klasifikasi')
                     y_pred = model.predict(X, verbose=0)
                     y_pred = np.argmax(y_pred, axis=1)
                     df['pred'] = y_pred
                     polarity_encode = {0: 'Negatif', 1: 'Netral', 2: 'Positif'}
                     df['pred'] = df['pred'].map(polarity_encode).values
-
-                    st.write(f'Total Sentimen : {df.shape[0]}')
                     max_value = int(df['pred'].value_counts().argmax())
                     # st.write(max_value)
                     label_sentimen = ''
@@ -98,30 +89,20 @@ elif selectbox == 'csv':
                         label_sentimen = 'Netral'
                     else:
                         label_sentimen = 'Negatif'
+                    # my_bar.progress(1.0, 'selesai!')
                     labels = ['Negatif', 'Positif', 'Netral']
                     color_bar = ['red', 'green', 'gray']
                     counts = df['pred'].value_counts()
-                    fig, ax = plt.subplots(figsize=(6, 6))
-                    # sizes = [row for row in df['pred'].value_counts()]
                     sizes = [counts[0], counts[1], counts[2]]
-                    label = list(df['pred'].value_counts().index)
-                    explode = (0.1, 0, 0)
-                    ax.pie(x=counts.values, labels=labels, colors=color_bar,
-                           autopct='%1.1f%%',
-                           textprops={'fontsize': 11})
-                    ax.set_title('sentimen terhadap sistem tilang elektronik di twitter', fontsize=12)
-
-                    fig_bar, ax_bar = plt.subplots()
-                    ax_bar.bar(counts.index, counts.values, color=color_bar)
-                    for i in range(len(counts.values)):
-                        ax_bar.text(i, sizes[i], sizes[i], ha='center')
-                    ax_bar.set_xlabel('Nilai Prediksi')
-                    ax_bar.set_ylabel('Jumlah')
-                    ax_bar.set_title('Diagram Batang Prediksi')
-                    # legend = ax_bar.legend(labels=labels, loc='best')
-                    st.write(f'Sentimen Terbanyak : {label_sentimen}')
-                    st.pyplot(fig)
+                    st.subheader(f'Total Sentimen : {df.shape[0]}')
+                    st.subheader(f'Sentimen Terbanyak : {label_sentimen}')
+                    fig_pie, ax_pie = pie_chart(x=counts.values, label=labels, color=color_bar)
+                    fig_bar, ax_bar = bar_chart(x=counts.index, height=counts.values, sizes=sizes, color=color_bar)
+                    # pie_image = pie_chart(x=counts.values, label=labels, color=color_bar)
+                    # bar_image = bar_chart(x=counts.index, height=counts.values, sizes=sizes,color=color_bar)
+                    st.pyplot(fig_pie)
                     st.pyplot(fig_bar)
                     simpan = st.button('Simpan', disabled=True)
-                except:
-                    st.error('File tidak sesuai atau rusak')
+
+            except:
+                st.error('File tidak sesuai atau rusak')
