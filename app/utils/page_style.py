a6d9e9294b6aca518338686eebab249c4f9a22e4
anandadwii <namikaze.anandadwi@gmail.com>
2023-03-24 14:27:20 +0700

    chore : simplify css command

--- app/utils/page_style.py
+++ app/utils/page_style.py
@@ -1,24 +1,10 @@
 import streamlit as st
-
-hide_streamlit_style = """"
-<style>
-	/* This is to hide hamburger menu completely */
-	#MainMenu {visibility: hidden;}
-	/* This is to hide Streamlit footer */
-	footer {visibility: hidden;}
-	/*
-	If you did not hide the hamburger menu completely,
-	you can use the following styles to control which items on the menu to hide.
-	*/
-	ul[data-testid=main-menu-list] > li:nth-of-type(4), /* Documentation */
-	ul[data-testid=main-menu-list] > li:nth-of-type(5), /* Ask a question */
-	ul[data-testid=main-menu-list] > li:nth-of-type(6), /* Report a bug */
-	ul[data-testid=main-menu-list] > li:nth-of-type(7), /* Streamlit for Teams */
-	ul[data-testid=main-menu-list] > div:nth-of-type(2) /* 2nd divider */
-		{display: none;}
-</style>
-
-"""
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
 
 
 def remove_footer():
