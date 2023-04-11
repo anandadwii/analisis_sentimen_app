5dce30e20fda91c4cd64704fa69a643a12be7c67
anandadwii <namikaze.anandadwi@gmail.com>
2023-04-11 08:16:36 +0700

    chore : change size of figure

--- app/utils/charts.py
+++ app/utils/charts.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 
 def pie_chart(x, label, color, is_explode: bool = False):
-    fig, ax = plt.subplots(figsize=(5, 5))
+    fig, ax = plt.subplots(figsize=(8, 8))
     explode = (0.1, 0, 0)
     if is_explode:
         ax.pie(x=x, labels=label, explode=explode, colors=color,
@@ -12,12 +12,12 @@ def pie_chart(x, label, color, is_explode: bool = False):
         ax.pie(x=x, labels=label, colors=color,
                autopct='%1.1f%%',
                textprops={'fontsize': 11})
-    ax.set_title('sentimen terhadap sistem tilang elektronik di twitter', fontsize=12)
+    # ax.set_title('sentimen terhadap sistem tilang elektronik di twitter', fontsize=12)
     return fig, ax
 
 
 def bar_chart(x, height, sizes, color):
-    fig, ax = plt.subplots(figsize=(5, 5))
+    fig, ax = plt.subplots(figsize=(8, 8))
     ax.bar(x, height, color=color)
     for i in range(len(height)):
         ax.text(i, sizes[i], sizes[i], ha='center')
