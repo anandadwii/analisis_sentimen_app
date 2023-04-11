a917910fbf1738df79f11f452b48224b972fc50b
anandadwii <namikaze.anandadwi@gmail.com>
2023-04-11 08:17:19 +0700

    add : live sentiment analysis from twitter

--- app/pages/main_page_alt.py
+++ app/pages/main_page_alt.py
@@ -1,15 +1,20 @@
 # allow python to jump read another python file
 import os
 import sys
+import traceback
+
 import streamlit as st
 import pickle
 import numpy as np
 from keras.utils import pad_sequences
 import pandas as pd
+import snscrape.modules.twitter as crawler
+
 sys.path.insert(1, os.getcwd())
 from app.utils.cleaner import *
 from app.utils.charts import pie_chart, bar_chart
 import app.utils.page_style as ps
+
 seed = 10
 np.random.seed(seed)
 # start render
@@ -23,24 +28,24 @@ st.markdown(
     Model yang digunakan adalah LSTM.
     """
 )
-# raw_text_state = False
-# if raw_text_state:
-#     selectbox = st.selectbox("Silahkan pilih", ("", "Raw Text", "csv"))
-# else:
-#     selectbox = st.selectbox("Silahkan pilih", ("", "csv"))
-
+raw_text_state = True
+if raw_text_state:
+    selectbox = st.selectbox("Silahkan pilih", ("", "Raw Text", "csv", "live sentiment"))
+else:
+    selectbox = st.selectbox("Silahkan pilih", ("", "csv"))
 
 model = model_loader()
 with open('app/resources/tokenizer/tokenizer.pickle', 'rb') as handler:
     tokenizer = pickle.load(handler)
 
-# if selectbox == "Raw Text":
-#     input_text = st.text_area(label='Sentimen', max_chars=100, height=100)
-#     trigger = st.button('Analisis!')
-#     if trigger:
-#         clean_text = preprocessing_data(input_text)
-#         sequences = tokenizer.texts_to_sequences([clean_text])
-#         data = pad_sequences(sequences, maxlen=53)
+if selectbox == "Raw Text":
+    input_text = st.text_area(label='Sentimen', max_chars=100, height=100)
+    trigger = st.button('Analisis!')
+    if trigger:
+        clean_text = preprocessing_data(input_text)
+        sequences = tokenizer.texts_to_sequences([clean_text])
+        data = pad_sequences(sequences, maxlen=53)
+        print(data)
 #         # prediction = model.predict(np.array(data, ndmin=2))
 #         prediction = model.predict([data], verbose=0)
 #         prediction_prob_negative = prediction[0][0]
@@ -58,22 +63,65 @@ with open('app/resources/tokenizer/tokenizer.pickle', 'rb') as handler:
 #             st.success(f'Sentimen bernilai Postif, {prediction_prob_positive}')
 #
 #
-# elif selectbox == 'csv':
-file = st.file_uploader(label="Unggah CSV dengan separator ;", type="csv", accept_multiple_files=False)
-stop_word_check = st.checkbox('remove stopword', disabled=True, value=True)
-trigger = st.button('Unggah')
-if trigger and file is not None:
-    try:
-        with st.spinner('Harap Tunggu'):
-            # my_bar = st.progress(0.0, text='sedang membaca csv')
-            df = pd.read_csv(file, sep=';')
-            df['processed'] = df.iloc[:, 0]
-            # my_bar.progress(0.25, 'sedang praproses csv')
-            X = df['processed'].apply(preprocessing_data)
-            # my_bar.progress(0.50, 'sedang tokenisasi')
+elif selectbox == 'csv':
+    file = st.file_uploader(label="Unggah CSV dengan separator ;", type="csv", accept_multiple_files=False)
+    # stop_word_check = st.checkbox('remove stopword', disabled=True, value=True)
+    trigger = st.button('Unggah')
+    if trigger and file is not None:
+        try:
+            with st.spinner('Harap Tunggu'):
+                # my_bar = st.progress(0.0, text='sedang membaca csv')
+                df = pd.read_csv(file, sep=';')
+                df['processed'] = df.iloc[:, 0]
+                X = df['processed'].apply(preprocessing_data)
+                X = tokenizer.texts_to_sequences(X.values)
+                X = pad_sequences(X, maxlen=53)
+                y_pred = model.predict(X, verbose=0)
+                y_pred = np.argmax(y_pred, axis=1)
+                df['pred'] = y_pred
+                polarity_encode = {0: 'Negatif', 1: 'Netral', 2: 'Positif'}
+                df['pred'] = df['pred'].map(polarity_encode).values
+                max_value = int(df['pred'].value_counts().argmax())
+                # st.write(max_value)
+                label_sentimen = polarity_encode.get(max_value)
+                # my_bar.progress(1.0, 'selesai!')
+                labels = ['Negatif', 'Positif', 'Netral']
+                color_bar = ['red', 'green', 'gray']
+                counts = df['pred'].value_counts()
+                sizes = [counts[0], counts[1], counts[2]]
+                st.write('#')
+                st.subheader(f'Total Sentimen : {df.shape[0]}')
+                st.write('#')
+                st.subheader(f'Sentimen Terbanyak : {label_sentimen}')
+                st.write('#')
+                fig_pie, ax_pie = pie_chart(x=counts.values, label=labels, color=color_bar)
+                fig_bar, ax_bar = bar_chart(x=counts.index, height=counts.values, sizes=sizes, color=color_bar)
+                st.pyplot(fig_pie)
+                st.pyplot(fig_bar)
+                save = st.button('Simpan', disabled=True)
+
+        except:
+            st.error('File tidak sesuai atau rusak')
+
+elif selectbox == 'live sentiment':
+    with st.form(key='scrape_form'):
+        query_keyword = st.text_input(label='keyword for queries')
+        start = st.date_input(label='start date')
+        end = st.date_input(label='end date')
+        submit_button = st.form_submit_button(label="Submit")
+    if submit_button:
+        query_input = f'{query_keyword} until:{end} since:{start}'
+        # print(query_input)
+        tweet_list = []
+        try:
+            for i, tweet in enumerate(crawler.TwitterSearchScraper(query_input).get_items()):
+                clean_tweet = preprocessing_data(tweet.content)
+                tweet_list.append(clean_tweet)
+            _result = list(set(tweet_list))
+            df = pd.DataFrame(_result, columns=['processed'])
+            X = df['processed']
             X = tokenizer.texts_to_sequences(X.values)
             X = pad_sequences(X, maxlen=53)
-            # my_bar.progress(0.75, 'sedang melakukan klasifikasi')
             y_pred = model.predict(X, verbose=0)
             y_pred = np.argmax(y_pred, axis=1)
             df['pred'] = y_pred
@@ -86,19 +134,27 @@ if trigger and file is not None:
             labels = ['Negatif', 'Positif', 'Netral']
             color_bar = ['red', 'green', 'gray']
             counts = df['pred'].value_counts()
-            sizes = [counts[0], counts[1], counts[2]]
+            sizes = [0, 0, 0]
+            for x in counts.keys():
+                if x == 'Positif':
+                    sizes[1] = counts.get('Positif')
+                elif x == 'Negatif':
+                    sizes[0] = counts.get('Negatif')
+                elif x == 'Netral':
+                    sizes[2] = counts.get('Netral')
             st.write('#')
             st.subheader(f'Total Sentimen : {df.shape[0]}')
             st.write('#')
             st.subheader(f'Sentimen Terbanyak : {label_sentimen}')
             st.write('#')
-            fig_pie, ax_pie = pie_chart(x=counts.values, label=labels, color=color_bar)
+            fig_pie, ax_pie = pie_chart(x=sizes, label=labels, color=color_bar)
             fig_bar, ax_bar = bar_chart(x=counts.index, height=counts.values, sizes=sizes, color=color_bar)
-            # pie_image = pie_chart(x=counts.values, label=labels, color=color_bar)
-            # bar_image = bar_chart(x=counts.index, height=counts.values, sizes=sizes,color=color_bar)
             st.pyplot(fig_pie)
             st.pyplot(fig_bar)
-            save = st.button('Simpan', disabled=True)
 
-    except:
-        st.error('File tidak sesuai atau rusak')
+
+        except:
+            traceback.print_exc()
+
+
+# ToDo: fix value tiap sentimen, masih berbeda. warna ama sentimen terbanyak masih ga sinkron
