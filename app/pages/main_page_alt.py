1f8452e6b9379216aaada78ce2aabd4fc91dae49
anandadwii <namikaze.anandadwi@gmail.com>
2023-06-26 01:10:41 +0700

    fix: read csv, add download csv button and remove Neutral statement

--- app/pages/main_page_alt.py
+++ app/pages/main_page_alt.py
@@ -31,7 +31,7 @@ st.markdown(
 )
 raw_text_state = True
 if raw_text_state:
-    selectbox = st.selectbox("Silahkan pilih", ("", "Raw Text", "csv", "live sentiment"))
+    selectbox = st.selectbox("Silahkan pilih", ("", "csv", "live sentiment"))
 else:
     selectbox = st.selectbox("Silahkan pilih", ("", "csv"))
 
@@ -46,21 +46,21 @@ if selectbox == "Raw Text":
         sequences = tokenizer.texts_to_sequences([clean_text])
         data = pad_sequences(sequences, maxlen=53)
         print(data)
-#         # prediction = model.predict(np.array(data, ndmin=2))
-#         prediction = model.predict([data], verbose=0)
-#         prediction_prob_negative = prediction[0][0]
-#         prediction_prob_neutral = prediction[0][1]
-#         prediction_prob_positive = prediction[0][2]
-#         prediction_class = prediction.argmax(axis=-1)[0]
-#         # prediction_class = np.argmax(prediction, axis=1)
-#         # print(prediction_class )
-#         # st.header('Prediction using LSTM model')
-#         if prediction_class == 0:
-#             st.error(f'Sentimen bernilai Negatif, {prediction_prob_negative}')
-#         if prediction_class == 1:
-#             st.info(f'Sentimen bernilai Netral, {prediction_prob_neutral}')
-#         if prediction_class == 2:
-#             st.success(f'Sentimen bernilai Postif, {prediction_prob_positive}')
+        # # prediction = model.predict(np.array(data, ndmin=2))
+        # prediction = model.predict([data], verbose=0)
+        # prediction_prob_negative = prediction[0][0]
+        # prediction_prob_neutral = prediction[0][1]
+        # prediction_prob_positive = prediction[0][2]
+        # prediction_class = prediction.argmax(axis=-1)[0]
+        # # prediction_class = np.argmax(prediction, axis=1)
+        # # print(prediction_class )
+        # # st.header('Prediction using LSTM model')
+        # if prediction_class == 0:
+        #     st.error(f'Sentimen bernilai Negatif, {prediction_prob_negative}')
+        # if prediction_class == 1:
+        #     st.info(f'Sentimen bernilai Netral, {prediction_prob_neutral}')
+        # if prediction_class == 2:
+        #     st.success(f'Sentimen bernilai Postif, {prediction_prob_positive}')
 #
 #
 elif selectbox == 'csv':
@@ -72,7 +72,10 @@ elif selectbox == 'csv':
         with st.spinner('Harap Tunggu'):
             # my_bar = st.progress(0.0, text='sedang membaca csv')
             try:
-                df = pd.read_csv(file, header=None, usecols=[0])
+                try:
+                    df = pd.read_csv(file, header=None, usecols=[0])
+                except:
+                    pass
                 df['processed'] = df[0]
                 X = df['processed'].apply(preprocessing_data)
                 X = tokenizer.texts_to_sequences(X.values)
@@ -84,19 +87,19 @@ elif selectbox == 'csv':
                 df['pred'] = df['pred'].map(polarity_encode).values
                 max_value = int(df['pred'].value_counts().argmax())
                 label_sentimen = polarity_encode.get(max_value)
-                labels = ['Negatif', 'Positif', 'Netral']
-                color_bar = ['red', 'green', 'gray']
+                labels = ['Negatif', 'Positif']
+                color_bar = ['red', 'green']
                 counts = df['pred'].value_counts()
                 # print(counts)
                 # sizes = [counts[0], counts[1], counts[2]]
-                sizes = [0, 0, 0]
+                sizes = [0, 0]
                 for key, value in df['pred'].value_counts().items():
                     if key == 'Positif':
                         sizes[1] = value
                     elif key == 'Negatif':
                         sizes[0] = value
-                    elif key == 'Netral':
-                        sizes[2] = value
+                    # elif key == 'Netral':
+                    #     sizes[2] = value
                 dict_data = {label: size for label, size in zip(labels, sizes)}
                 st.write('#')
                 st.subheader(f'Total Sentimen : {df.shape[0]}')
@@ -120,59 +123,73 @@ elif selectbox == 'csv':
 
 elif selectbox == 'live sentiment':
     with st.form(key='scrape_form'):
-        query_keyword = st.text_input(label='keyword for queries')
-        start = st.date_input(label='start date')
-        end = st.date_input(label='end date')
+        query_keyword = st.text_input(label='keyword untuk melakukan query')
+        start = st.date_input(label='tanggal mulai')
+        end = st.date_input(label='tanggal selesai')
         submit_button = st.form_submit_button(label="Submit")
     if submit_button:
-        query_input = f'{query_keyword} until:{end} since:{start}'
-        # print(query_input)
-        tweet_list = []
-        try:
-            with st.spinner('Harap Tunggu'):
-                for i, tweet in enumerate(crawler.TwitterSearchScraper(query_input).get_items()):
-                    clean_tweet = preprocessing_data(tweet.content)
-                    tweet_list.append(clean_tweet)
-                _result = list(set(tweet_list))
-                df = pd.DataFrame(_result, columns=['processed'])
-                X = df['processed']
-                X = tokenizer.texts_to_sequences(X.values)
-                X = pad_sequences(X, maxlen=57)
-                y_pred = model.predict(X, verbose=0)
-                y_pred = np.argmax(y_pred, axis=1)
-                df['pred'] = y_pred
-                polarity_encode = {0: 'Negatif', 1: 'Netral', 2: 'Positif'}
-                df['pred'] = df['pred'].map(polarity_encode).values
-                # max_value = int(df['pred'].value_counts().argmax())
-                # label_sentimen = polarity_encode.get(max_value)
-                labels = ['Negatif', 'Positif', 'Netral']
-                color_bar = ['red', 'green', 'gray']
-                # counts = df['pred'].value_counts()
-                sizes = [0, 0, 0]
-                for key, value in df['pred'].value_counts().items():
-                    if key == 'Positif':
-                        sizes[1] = value
-                    elif key == 'Negatif':
-                        sizes[0] = value
-                    elif key == 'Netral':
-                        sizes[2] = value
+        if start == end:
+            st.error("tanggal mulai dan tanggal selesai tidak boleh sama")
+        if len(query_keyword) == 0:
+            st.error("keyword untuk query tidak boleh kosong")
+        else:
+            query_input = f'{query_keyword} until:{end} since:{start}'
+            # print(query_input)
+            tweet_list = []
+            try:
+                with st.spinner('Harap Tunggu'):
+                    for i, tweet in enumerate(crawler.TwitterSearchScraper(query_input).get_items()):
+                        clean_tweet = preprocessing_data(tweet.content)
+                        tweet_list.append(clean_tweet)
+                    _result = list(set(tweet_list))
+                    df = pd.DataFrame(_result, columns=['processed'])
+                    X = df['processed']
+                    X = tokenizer.texts_to_sequences(X.values)
+                    X = pad_sequences(X, maxlen=57)
+                    y_pred = model.predict(X, verbose=0)
+                    y_pred = np.argmax(y_pred, axis=1)
+                    df['pred'] = y_pred
+                    polarity_encode = {0: 'Negatif',
+                                       1: 'Netral',
+                                       2: 'Positif'}
+                    df['pred'] = df['pred'].map(polarity_encode).values
+                    df = df[df['pred'] != 'Netral']
+                    labels = ['Negatif', 'Positif']
+                    color_bar = ['red', 'green']
+                    sizes = [0, 0]
+                    for key, value in df['pred'].value_counts().items():
+                        if key == 'Positif':
+                            sizes[1] = value
+                        elif key == 'Negatif':
+                            sizes[0] = value
+                        # elif key == 'Netral':
+                        #     sizes[2] = value
 
-                dict_data = {label: size for label, size in zip(labels, sizes)}
-                st.write('#')
-                st.subheader(f'Total Sentimen : {df.shape[0]}')
-                st.write('#')
-                highest_sentiment = max(dict_data, key=dict_data.get)
-                paragraph = f"Berdasarkan input keyword {query_keyword} mulai {start} hingga {end} menghasilkan " \
-                            f"sebaran sentimen terbanyak yaitu {highest_sentiment}"
-                st.write(paragraph)
-                st.write('#')
-                fig_pie, ax_pie = pie_chart(x=sizes, label=labels, color=color_bar)
-                fig_bar, ax_bar = bar_chart(x=labels, height=sizes, sizes=sizes, color=color_bar)
-                st.pyplot(fig_pie)
-                st.pyplot(fig_bar)
-                st.download_button(label="Download as pptx",
-                                   data=create_ppt(dict_data, paragraph),
-                                   file_name=f"Report Live Sentiment.pptx")
-        except:
-            st.write(traceback.print_exc())
-            traceback.print_exc()
+                    dict_data = {label: size for label, size in zip(labels, sizes)}
+                    st.write('#')
+                    st.subheader(f'Total Sentimen : {sum(sizes)}')
+                    st.write('#')
+                    highest_sentiment = max(dict_data, key=dict_data.get)
+                    paragraph = f"Berdasarkan input keyword {query_keyword} mulai {start} hingga {end} menghasilkan " \
+                                f"sebaran sentimen terbanyak yaitu {highest_sentiment}"
+                    st.write(paragraph)
+                    st.write('#')
+                    fig_pie, ax_pie = pie_chart(x=sizes, label=labels, color=color_bar)
+                    fig_bar, ax_bar = bar_chart(x=labels, height=sizes, sizes=sizes, color=color_bar)
+                    st.pyplot(fig_pie)
+                    st.pyplot(fig_bar)
+                    st.download_button(
+                        label="Download as pptx",
+                        data=create_ppt(dict_data, paragraph),
+                        file_name=f"Report {str(selectbox)}.pptx")
+                    st.download_button(
+                        label="Download as csv",
+                        data=convert_df(df),
+                        file_name=f"datasheet.csv",
+                        mime="text/csv",
+                        key='download-csv'
+                    )
+
+            except:
+                st.error(traceback.print_exc())
+                traceback.print_exc()
